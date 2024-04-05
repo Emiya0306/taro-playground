e914ccc4386b3e4ef60d725d221acc76600d2d22
Jie Luo <worfandsheep@gmail.com>
2024-04-05 10:36:29 +0800

    support react native

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,4 +1,4 @@
-import { View, Text } from '@tarojs/components'
+import { View, Text, Input, Form } from '@tarojs/components'
 import { useLoad } from '@tarojs/taro'
 import './index.less'
 
@@ -10,7 +10,17 @@ export default function Index() {
 
   return (
     <View className='index'>
-      <Text>Hello world!</Text>
+      <Text>Hello world!1</Text>
+      <Form>
+        <View className='form-item username'>
+          <Text>Username:</Text>
+          <Input className='username-input' type='text' />
+        </View>
+        <View className='form-item password'>
+          <Text>Password:</Text>
+          <Input className='password-input' type='safe-password' password />
+        </View>
+      </Form>
     </View>
   )
 }
